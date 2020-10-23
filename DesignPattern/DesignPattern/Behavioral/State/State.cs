d116d612172a7bd914e3d4a8d849d9d15033e8e4
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-10-23 21:51:05 +0600

    state

---
+++ DesignPattern/DesignPattern/Behavioral/State/State.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Behavioral.State
+{
+   // 2 Components 1) Context 2) State.Context hold State reference.
+
+    /// <summary>
+    /// state interface
+    /// </summary>
+    public interface IState
+    {
+        void Handle(VideoGame game);
+    }
+
+    public class StaredState : IState
+    {
+        public void Handle(VideoGame game)
+        {
+            if (!(game.CurrentState is stopState))
+            {
+               // game.ChangeState(new StaredState());
+            }
+        }
+    }
+    public class PausedState : IState
+    {
+        public void Handle(VideoGame game)
+        {
+            if (!(game.CurrentState is PausedState))
+            {
+                //game.ChangeState(new PausedState());
+            }
+        }
+    }
+    public class stopState : IState
+    {
+        public void Handle(VideoGame game)
+        {
+            if(!(game.CurrentState is stopState))
+            {
+                //game.ChangeState(new stopState());
+            }
+        }
+    }
+
+    ///<summary>
+    ///context class
+    ///</summary>
+    public class VideoGame
+    {
+        public IState CurrentState { get; private set; }
+        public void ChangeState(IState state)
+        {
+            this.CurrentState = state;
+        }
+        public void Operate()
+        {
+            CurrentState.Handle(this);
+        }
+    }
+}
---
+++ DesignPattern/DesignPattern/Behavioral/State/StateTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DesignPattern.Behavioral.State
+{
+    public class StateTest
+    {
+        [Fact]
+        public void ShouldStartGame()
+        {
+            var videogame = new VideoGame();
+            var startState = new StaredState();
+            videogame.ChangeState(startState);
+            videogame.Operate();
+            videogame.CurrentState.Should().BeEquivalentTo(startState);
+        }
+    }
+}
