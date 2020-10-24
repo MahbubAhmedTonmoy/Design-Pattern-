a965ec7c9bcb2a9c5cdcd83f61110be0da5a5016
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-10-24 12:15:19 +0600

    adapter

---
+++ DesignPattern/DesignPattern/Structural/Adapter/Adapter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPattern.Structural.Adapter
+{
+    public interface IClientTarget
+    {
+        int Sum(int[] numbers);
+    }
+
+    public class Adapter: IClientTarget
+    {
+        private readonly IAdaptee adaptee;
+        public Adapter(IAdaptee adaptee)
+        {
+            this.adaptee = adaptee;
+        }
+
+        public int Sum(int[] numbers)
+        {
+            return adaptee.Sum(numbers.ToList());
+        }
+    }
+
+    public interface IAdaptee
+    {
+        int Sum(List<int> numbers);
+    }
+    public class Adaptee : IAdaptee
+    {
+        public int Sum(List<int> numbers)
+        {
+            var ans = 0;
+            foreach (int i in numbers)
+            {
+                ans += i;
+            }
+            return ans;
+        }
+    }
+}
---
+++ DesignPattern/DesignPattern/Structural/Adapter/AdapterTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DesignPattern.Structural.Adapter
+{
+    public class AdapterTest
+    {
+       
+        [Fact]
+        public void SholdReturnSum()
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4 };
+            var adapter = new Adapter(new Adaptee());
+            var total = adapter.Sum(numbers);
+            total.Should().Be(10);
+        }
+    }
+}
