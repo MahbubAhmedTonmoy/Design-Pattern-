7aa4e28853c9f9259f553444772eabf53f29fd54
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-10-26 10:26:10 +0600

    singleton

---
+++ DesignPattern/DesignPattern/Creational/SingleTon/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Creational.SingleTon
+{
+    public class Singleton
+    {
+        public Guid ApplicationId { get; private set; }
+        private static Singleton instance = null;
+        private static object lockobject = new object();
+
+        public Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if(instance == null)  //double checked lock
+            {
+                lock (lockobject)
+                {
+                    if(instance == null)
+                    {
+                        instance = new Singleton
+                        {
+                            ApplicationId = Guid.NewGuid()
+                        };
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+}
---
+++ DesignPattern/DesignPattern/Creational/SingleTon/SingletonTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DesignPattern.Creational.SingleTon
+{
+    public class SingletonTest
+    {
+
+        [Fact]
+        public void ShoudWork()
+        {
+            Singleton singleTon = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+            singleTon.Should().NotBeNull();
+            singleTon.ApplicationId.Should().NotBeEmpty();
+        }
+    }
+}
