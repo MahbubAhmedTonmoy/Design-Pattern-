501146bdd256e9bdfae5b2948f4f4921d224b851
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-10-27 13:50:24 +0600

    proxy

---
+++ DesignPattern/DesignPattern/Structural/Proxy/Proxy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Structural.Proxy
+{
+    class Proxy
+    {
+    }
+
+    public interface IBank
+    {
+        double Withdraw(double ammount);
+    }
+
+    public class BankService : IBank
+    {
+        public double Withdraw(double ammount)
+        {
+            return ammount;
+        }
+
+        
+    }
+
+    public class ATMService : IBank
+    {
+        private readonly IBank bank = new BankService();
+
+        public double Withdraw(double ammount)
+        {
+            if(ammount > 25000)
+            {
+                bank.Withdraw(ammount);
+            }
+            return ammount;
+        }
+    }
+}
---
+++ DesignPattern/DesignPattern/Structural/Proxy/ProxyTest.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DesignPattern.Structural.Proxy
+{
+    public class ProxyTest
+    {
+        [Fact]
+        public void ShouldWorkProxy()
+        {
+            IBank bank = new ATMService();
+            var proxy = bank.Withdraw(1000);
+            var real = bank.Withdraw(26000);
+            proxy.Should().Be(1000);
+            real.Should().Be(26000);
+        }
+    }
+}
