b69c03c4823d978a9a8e3f88af835997f01d7bf4
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-11-14 17:32:56 +0600

    null objcet pattern

---
+++ DesignPattern/DesignPattern/Behavioral/NullObject/NullObject.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPattern.Behavioral.NullObject
+{
+    
+    public interface IUser<T>
+    {
+        string GetUserRole();
+    }
+    public class User : IUser<User>
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+
+        public string GetUserRole()
+        {
+            return Id;
+        }
+    }
+
+    public class NullUser : IUser<User>
+    {
+        public string GetUserRole()
+        {
+            return $"not found";
+        }
+    }
+
+    public class UserRepository
+    {
+        private static List<User> PopulatedUserList()
+        {
+            List<User> userList = new List<User>(); 
+            userList.Add(new User
+            {
+                Name = "Zodu",
+                Id = "bit0215"
+            });
+            return userList;
+        }
+
+        public static IUser<User> GetUserByName(string name)
+        {
+            List<User> users = PopulatedUserList();
+            var user = users.Where(x => x.Name == name).FirstOrDefault();
+            if(user == null)
+            {
+                return new NullUser();
+            }
+            return user;
+        } 
+    }
+
+}
---
+++ DesignPattern/DesignPattern/Behavioral/NullObject/NullObjectTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DesignPattern.Behavioral.NullObject
+{
+    public class NullObjectTest
+    {
+        [Fact]
+        public void ShouldReturnNull()
+        {
+            IUser<User> user = UserRepository.GetUserByName("Modu");
+            user.GetUserRole().Should().Be("not found");
+        }
+        [Fact]
+        public void ShouldReturnUser()
+        {
+            IUser<User> user = UserRepository.GetUserByName("Zodu");
+            user.GetUserRole().Should().Be("bit0215");
+        }
+    }
+}
