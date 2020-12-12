92d75ad5f3021c84c2e1c94106da21d918c75377
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-12-12 09:58:28 +0600

    singleton and prototype

---
+++ DesignPattern/DesignPattern/Creational/Prototype/prototype.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Creational.Prototype
+{
+    /*
+     * একটা ব্যাপার আমরা সবাই জানি যে, আমরা যখন new ইউজ করে অবজেক্ট বানাই তখন সাথে সাথে অবজেক্টটি মেমরীতে জায়গা দখল করে বসে ।
+     */
+    /*
+     * সমাধানঃ এই বার বার অবেজক্ট বানানোর হাত থেকে রেহাই পেতে আমরা যা করতে পারি তা হচ্ছে একটি অবজেক্টের ক্লোন বানিয়ে রেখে দিতে পারি
+     *  যখনই ঐ অবজেক্টের দরকার হবে ঠিক তখনই আমরা এর ক্লোন ইউজ করতে পারবো । আমাদের নতুন করে আর অবজেক্ট বানানোর দরকার হবে না ।
+     */
+     
+    public interface IClone
+    {
+        object Clone();
+    }
+    public class Address
+    {
+        public string PostOffice { get; set; }
+    }
+    public class Person : IClone
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public object Clone()
+        {
+            /* ক্লোনিং এর উপায় নম্বর ১:
+             * Person এর নতুন অবজেক্ট বানিয়ে এই অবজেক্টের একটা একটা করে প্রোপার্টি ধরে ধরে আমাদের Person class এর প্রোপার্টি গুলো এসাইন করতে হবে 
+             */
+            var person = new Person
+            {
+                Name = Name,
+                Email = Email
+            };
+            return person;
+        }
+    }
+    public class Teacher: IClone
+    {
+        public string Name { get; set; }
+        public Address Address { get; set; }
+        public object Clone()
+        {
+            /*ক্লোনিং এর উপায় নম্বর ২: MemberwiseClone()
+             */
+            //return MemberwiseClone();
+            //cloning member variable
+            Teacher teacher = MemberwiseClone() as Teacher;
+            //cloning reference object
+            teacher.Address = new Address();
+            teacher.Address.PostOffice = this.Address.PostOffice;
+            return teacher;
+
+            /*Shallow copy vs Deep copy
+             * shallow : ShallowCopy আর কিছুই না, একটা অবজেক্ট এর প্রত্যকেটা প্রোপার্টি কে অন্য অবজেক্ট এর প্রোপার্টিতে এসাইন করে দেয়াই হচ্ছে ShallowCopy ।
+             * Deep:  সেটা হচ্ছে অবজেক্ট এর রেফারেন্সটাও কপি করে অবজেক্ট রিটার্ন করা
+             */
+
+        }
+    }
+}
--- DesignPattern/DesignPattern/Creational/SingleTon/Singleton.cs
+++ DesignPattern/DesignPattern/Creational/SingleTon/Singleton.cs
@@ -6,15 +6,22 @@ namespace DesignPattern.Creational.SingleTon
 {
     public class Singleton
     {
+        /*
+         * Thread Safe Singleton without using locks and no lazy instantiation
+         */
+        //private static readonly Singleton obj = new Singleton(); //Eagerly Created Instance
         public Guid ApplicationId { get; private set; }
         private static Singleton instance = null;
-        private static object lockobject = new object();
+        private static object lockobject = new object(); // multi thread safe
 
-        public Singleton()
+        private Singleton()
         {
 
         }
 
+        /***এই এপ্রোচে আমরা প্রথমে চেক করি যে আমাদের অবজেক্ট আগে বানানো হয়েছে কিনা ।
+         * যদি না হয়ে থাকে তাহলে আমরা lock block এ ঢুকি । lock block এ আবার আমরা চেক করি অবজেক্ট এখনও নাল কিনা । 
+         * নাল হলে আমরা নতুন অবজেক্ট বানিয়ে রিটার্ন করি । তাহলে আমাদের কোড হচ্ছে এ রকমঃ ****/
         public static Singleton GetInstance()
         {
             if(instance == null)  //double checked lock
