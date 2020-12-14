02f7be466ee4a216aa96afeebb07ad9116afdf04
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-12-14 11:33:05 +0600

    pub sub pattern

---
+++ DesignPattern/DesignPattern/Behavioral/PublisherSubScriber/PublisherSubScriber.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace DesignPattern.Behavioral.PublisherSubScriber
+{
+    class PublisherSubScriber
+    {
+    }
+    public interface IMessageBroker
+    {
+        void Subscribe(string topic, IMessageSubscriber subscriber);
+        void Publish(string topic, string message);
+    }
+
+    public class CustomMessageBroker : IMessageBroker
+    {
+        private IList<TopicSubscriber> topicSubscribers = new List<TopicSubscriber>();
+        public void Subscribe(string topic, IMessageSubscriber MessageSubscriber)
+        {
+            topicSubscribers.Add(new TopicSubscriber(topic, MessageSubscriber));
+        }
+
+        public void Publish(string topic, string message)
+        {
+            foreach(TopicSubscriber i in topicSubscribers)
+            {
+                if (!i.Topic.Equals(topic))
+                {
+                    break;
+                }
+                i.MessageSubscriber.OnMessage(message);
+            }
+        }
+    }
+    public class HeadTeacher
+    {
+        private readonly IMessageBroker messageBroker;
+        public HeadTeacher(IMessageBroker messageBroker)
+        {
+            this.messageBroker = messageBroker;
+        }
+        public void PublishNotic()
+        {
+            this.messageBroker.Publish("HEADSIR", "school closed for student until covid 19 gone");
+        }
+    }
+    public class AccountsManager : IMessageSubscriber
+    {
+        public void OnMessage(string message)
+        {
+            ReceivedMessage(message);
+        }
+        private void ReceivedMessage(string message)
+        {
+            Debug.WriteLine($"Message={message}");
+        }
+    }
+    public class Student : IMessageSubscriber
+    {
+        public void OnMessage(string message)
+        {
+            ReceivedMessage(message);
+        }
+        private void ReceivedMessage(string message)
+        {
+            Debug.WriteLine($"Message={message}");
+        }
+    }
+    public interface IMessageSubscriber
+    {
+        void OnMessage(string message);
+    }
+    public class TopicSubscriber
+    {
+        public string Topic { get; private set; }
+        public IMessageSubscriber MessageSubscriber { get; private set; }
+        public TopicSubscriber(string topic, IMessageSubscriber messageSubscriber)
+        {
+            this.Topic = topic;
+            this.MessageSubscriber = messageSubscriber;
+        }
+    }
+}
---
+++ DesignPattern/DesignPattern/Behavioral/PublisherSubScriber/PublisherSubScriberTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DesignPattern.Behavioral.PublisherSubScriber
+{
+    public class PublisherSubScriberTest
+    {
+        [Fact]
+        public void ShouldPublishAndSubscibeMessageUsingPubSubDesignPattern()
+        {
+            IMessageBroker messageBroker = new CustomMessageBroker();
+
+            IMessageSubscriber accounts = new AccountsManager();
+            IMessageSubscriber students = new Student();
+
+            messageBroker.Subscribe("HEADSIR", accounts);
+            messageBroker.Subscribe("HEADSIR", students);
+
+            var headSir = new HeadTeacher(messageBroker);
+            headSir.PublishNotic();
+        }
+    }
+}
