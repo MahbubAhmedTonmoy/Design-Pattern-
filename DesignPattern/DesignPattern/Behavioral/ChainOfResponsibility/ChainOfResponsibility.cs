cdb5cf6f5d078d4bff0109430b978e5b1132f928
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-12-14 09:37:04 +0600

    chain of Responsiblity

--- DesignPattern/DesignPattern/Behavioral/ChainOfResponsibility/ChainOfResponsibility.cs
+++ DesignPattern/DesignPattern/Behavioral/ChainOfResponsibility/ChainOfResponsibility.cs
@@ -8,6 +8,105 @@ namespace DesignPattern.Behavioral.ChainOfResponsibility
     class ChainOfResponsibility
     {
     }
+    public class LoanRequest
+    {
+        public string NationalId { get; set; }
+        public bool PreviousRecord { get; set; }
+        public int LoanAmount { get; set; }
+        public bool Approved { get; set; }
+        public ILoanApprover ApprovedBy { get; set; }
+    }
 
-  
+    public class LoanExecutive
+    {
+        public LoanRequest RequestForLoanApprove(string nationaId, bool previousRecordGood, int loanAmount)
+        {
+            var loanRequest = new LoanRequest
+            {
+                NationalId = nationaId,
+                PreviousRecord = previousRecordGood,
+                LoanAmount = loanAmount
+            };
+            ILoanApprover loanApprover = new BranchManagerApprover("Mr. X");
+            loanApprover.Approve(loanRequest);
+            return loanRequest;
+        }
+    }
+
+    public interface ILoanApprover
+    {
+        public string GetApproverId();
+        void Approve(LoanRequest loanRequest);
+    }
+
+    public class BranchManagerApprover : ILoanApprover
+    {
+        private const int MaxLoanApprovedAmount = 1000;
+        private readonly ILoanApprover nextLoanApprover = new RegionallManagerApprover("Mr. Y");
+        private readonly string managerId = null;
+        public BranchManagerApprover(string managerId)
+        {
+            this.managerId = managerId;
+        }
+        public void Approve(LoanRequest loanRequest)
+        {
+            if(loanRequest.LoanAmount <= MaxLoanApprovedAmount)
+            {
+                loanRequest.Approved = true;
+                loanRequest.ApprovedBy = this;
+            }
+            nextLoanApprover.Approve(loanRequest);
+        }
+
+        public string GetApproverId()
+        {
+            return managerId;
+        }
+    }
+    
+    public class RegionallManagerApprover : ILoanApprover
+    {
+        private const int MaxLoanApprovedAmount = 10000;
+        private readonly ILoanApprover nextLoanApprover = new DevisionalManagerApprover("Mr. Z");
+        private readonly string managerId = null;
+        public RegionallManagerApprover(string managerId)
+        {
+            this.managerId = managerId;
+        }
+        public void Approve(LoanRequest loanRequest)
+        {
+            if (loanRequest.LoanAmount <= MaxLoanApprovedAmount)
+            {
+                loanRequest.Approved = true;
+                loanRequest.ApprovedBy = this;
+            }
+            nextLoanApprover.Approve(loanRequest);
+        }
+
+        public string GetApproverId()
+        {
+            return managerId;
+        }
+    }
+    public class DevisionalManagerApprover : ILoanApprover
+    {
+        private const int MaxLoanApprovedAmount = 100000;
+        private readonly string managerId = null;
+        public DevisionalManagerApprover(string managerId)
+        {
+            this.managerId = managerId;
+        }
+        public void Approve(LoanRequest loanRequest)
+        {
+            if (loanRequest.LoanAmount <= MaxLoanApprovedAmount && loanRequest.PreviousRecord)
+            {
+                loanRequest.Approved = true;
+                loanRequest.ApprovedBy = this;
+            }
+        }
+        public string GetApproverId()
+        {
+            return managerId;
+        }
+    }
 }
---
+++ DesignPattern/DesignPattern/Behavioral/ChainOfResponsibility/ChainOfResponsibilityTest.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DesignPattern.Behavioral.ChainOfResponsibility
+{
+    public class ChainOfResponsibilityTest
+    {
+        [Fact]
+        public void ShouldApproveLoanAmount()
+        {
+            var loanExecutive = new LoanExecutive();
+
+            LoanRequest loanRequest1 = loanExecutive.RequestForLoanApprove("123", false, 100);
+            LoanRequest loanRequest2 = loanExecutive.RequestForLoanApprove("234", true, 100000);
+
+            loanRequest1.Approved.Should().BeTrue();
+            loanRequest1.ApprovedBy.GetApproverId().Should().NotBeNullOrEmpty();
+
+            loanRequest2.Approved.Should().BeTrue();
+            loanRequest2.ApprovedBy.GetApproverId().Should().NotBeNullOrEmpty();
+        }
+    }
+}
