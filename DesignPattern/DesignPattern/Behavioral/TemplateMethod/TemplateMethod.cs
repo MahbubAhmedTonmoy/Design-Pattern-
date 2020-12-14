32b132ec22954d01ccf3d6c35492d2f83f84cb17
MahbubAhmedTonmoy <38882962+MahbubAhmedTonmoy@users.noreply.github.com>
2020-12-14 19:53:12 +0600

    templatemethod

---
+++ DesignPattern/DesignPattern/Behavioral/TemplateMethod/TemplateMethod.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Behavioral.TemplateMethod
+{
+    /// <summary>
+    /// এর আগে আমরা যারা স্ট্র্যাটেজি ডিজাইন প্যাটার্ন পড়েছি তারা একটা বিষয়ে আগে থেকেই ক্লিয়ার যে , কখনও কখনও আমাদের এপ্লিকেশনে 
+    /// এমন কিছু কাজ করি যার অ্যালগরিদম রান টাইমে চেঞ্জ করে ফেলা যায় । যেমন সর্টিং অ্যালগরিদম এর Array এর সাইজ হিসেব করে  আমরা 
+    /// রান টাইমে সর্টিং অ্যালগরিদম চেঞ্জ করে ফেলতে পারি ।
+    /// এখন আমাদের এপ্লিকেশন ডেভেলপের সময় এরকম প্রয়োজন হতে পারে যে, আমরা যে কাজটি(যেমন সর্টিং) করার জন্য অ্যালগরিদম ইউজ করব
+    /// সে অ্যালগরিদম ঠিকই থাকবে কিন্তু সেই অ্যালগরিদম এর কিছু স্টেপ পরিবর্তন হবে।
+    /// </summary>
+    public class TemplateMethod
+    {
+    }
+
+    public abstract class Document
+    {
+        protected StringBuilder document;
+        public Document(StringBuilder document)
+        {
+            this.document = document;
+        }
+        public string GenerateDocument()
+        {
+            CreateHeaderSection();
+            CreateBodySection();
+            CreateFooterSection();
+            return document.ToString();
+        }
+        protected abstract void CreateHeaderSection();
+        protected abstract void CreateBodySection();
+        protected abstract void CreateFooterSection();
+    }
+
+    public class XmlDocument : Document
+    {
+        public XmlDocument(StringBuilder document):base(document)
+        {
+
+        }
+        protected override void CreateBodySection()
+        {
+            document.Append($"<body>This is Document Body.</body>");
+        }
+
+        protected override void CreateFooterSection()
+        {
+            document.Append($"<footer>This is Document Footer.</footer></root>");
+        }
+
+        protected override void CreateHeaderSection()
+        {
+            document.Append($"<root><header>This is Document Header.</header>");
+        }
+    }
+
+    public class JsonDocument : Document
+    {
+        public JsonDocument(StringBuilder docuemnt) : base(docuemnt)
+        {
+        }
+
+        protected override void CreateBodySection()
+        {
+            document.Append(@"""body"":""This is Document Body."",");
+        }
+
+        protected override void CreateFooterSection()
+        {
+            document.Append(@"""footer"": ""This is Document Footer.""}");
+        }
+
+        protected override void CreateHeaderSection()
+        {
+            document.Append(@"{""header"": ""This is Document Header."",");
+        }
+    }
+}
---
+++ DesignPattern/DesignPattern/Behavioral/TemplateMethod/TemplateMethodTest.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+using Xunit;
+
+namespace DesignPattern.Behavioral.TemplateMethod
+{
+    public class TemplateMethodTest
+    {
+        [Fact]
+        public void ShoudCreateXmlDocument()
+        {
+            var documentBuilder = new StringBuilder();
+            Document document = new XmlDocument(documentBuilder);
+            var xmldata = document.GenerateDocument();
+
+            xmldata.Should().NotBeNullOrEmpty();
+            var xDoc = XDocument.Parse(xmldata);
+            xDoc.Should().NotBeNull();
+        }
+        [Fact]
+        public void ShoudCreateJsonDocument()
+        {
+            var documentBuilder = new StringBuilder();
+            Document document = new JsonDocument(documentBuilder);
+            var jsondata = document.GenerateDocument();
+
+            jsondata.Should().NotBeNullOrEmpty();
+            var jsonDoc = System.Text.Json.JsonDocument.Parse(jsondata);
+            jsonDoc.Should().NotBeNull();
+        }
+    }
+}
